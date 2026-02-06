635979d9811fcdb3a5aed0216910896a642e30af
Brody Manness <bmanness2@academic.rrc.ca>
2026-02-06 14:03:00 -0600

    Feat: Created daysAgo in tickets data.

--- src/data/tickets.ts
+++ src/data/tickets.ts
@@ -1,1 +1,7 @@
 import { Ticket } from "../api/v1/services/ticketService"
+
+const daysAgo = (days: number): string => {
+  const date = new Date();
+  date.setDate(date.getDate() - days);
+  return date.toISOString();
+};
