2fd61ad85bd65245449c8a2af52c3d80e9d1fbc3
Brody Manness <bmanness2@academic.rrc.ca>
2026-02-06 20:22:39 -0600

    Feat: Added getAllTickets controller.

--- src/api/v1/controllers/ticketsController.ts
+++ src/api/v1/controllers/ticketsController.ts
@@ -7,3 +7,13 @@ import {
     deleteTicket
 } from "../services/ticketsService";
 import { HTTP_STATUS } from "../../../constants/httpConstants";
+
+export const getAllTicketsController = (req: Request, res: Response) => {
+  const allTickets = getAllTickets();
+
+  res.status(HTTP_STATUS.OK).json({
+    message: "Tickets retrieved",
+    count: allTickets.length,
+    data: allTickets,
+  });
+};
