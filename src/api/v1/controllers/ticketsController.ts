cad40b075f447cb06036a17eaa83ad34dd774891
Brody Manness <bmanness2@academic.rrc.ca>
2026-02-06 20:25:54 -0600

    Feat: Added getTicketById.

--- src/api/v1/controllers/ticketsController.ts
+++ src/api/v1/controllers/ticketsController.ts
@@ -17,3 +17,19 @@ export const getAllTicketsController = (req: Request, res: Response) => {
     data: allTickets,
   });
 };
+
+export const getTicketByIdController = (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const ticket = getTicketById(id);
+
+  if (!ticket) {
+    return res.status(HTTP_STATUS.NOT_FOUND).json({
+      message: "Ticket not found",
+    });
+  }
+
+  res.status(HTTP_STATUS.OK).json({
+    message: "Ticket retrieved",
+    data: ticket,
+  });
+};
