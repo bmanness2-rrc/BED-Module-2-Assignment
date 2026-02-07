cc734960a703f5c45ad5b1d7bdfb9fa07f3b9d9c
Brody Manness <bmanness2@academic.rrc.ca>
2026-02-07 14:31:21 -0600

    Feat: Added routes to app.ts.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { Express } from "express";
+import ticketsRoutes from "./api/v1/routes/ticketsRoutes";
 
 // Initialize Express application
 const app: Express = express();
+
+app.use(express.json());
 
 app.get("/api/v1/health", (req, res) => {
     res.json({
@@ -11,5 +14,7 @@ app.get("/api/v1/health", (req, res) => {
         version: "1.0.0",
     });
 });
+
+app.use("/api/v1", ticketsRoutes);
 
 export default app;
