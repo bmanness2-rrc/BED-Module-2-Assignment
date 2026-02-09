3251ab481dc29af26840e029ad98d8b3e190a710
Brody Manness <bmanness2@academic.rrc.ca>
2026-02-08 19:28:53 -0600

    Feat: Created ticket service test file for the urgency function.

---
+++ test/ticketService.test.ts
@@ -0,0 +1,7 @@
+import {
+  calculateTicketAge,
+  calculateUrgencyScore,
+  getUrgencyLevel,
+} from "../src/api/v1/services/urgencyService";
+import { Ticket } from "../src/api/v1/services/ticketsService";
+
