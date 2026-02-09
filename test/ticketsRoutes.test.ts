e6a2fd29ae5bf35ab3c8c3e9938112df423b3968
Brody Manness <bmanness2@academic.rrc.ca>
2026-02-08 18:52:36 -0600

    Fix: Rewrote test page to match notes.

--- test/ticketsRoutes.test.ts
+++ test/ticketsRoutes.test.ts
@@ -1,32 +1,5 @@
 import request from "supertest";
-import express, { Express } from "express";
-import ticketsRouter from "../src/api/v1/routes/ticketsRoutes";
-import * as ticketsService from "../src/api/v1/services/ticketsService";
-import { HTTP_STATUS } from "../src/constants/httpConstants";
+import app from "../src/app";
+import * as ticketControllers from "../src/api/v1/controllers/ticketsController";
+import * as urgencyController from "../src/api/v1/controllers/urgencyController";
 
-const app: Express = express();
-app.use(express.json());
-app.use("/api/v1", ticketsRouter);
-
-jest.mock("../src/api/v1/services/ticketsService");
-
-describe("Ticket Routes", () => {
-    const mockTickets = [
-    {
-      id: 1,
-      title: "Test Ticket 1",
-      description: "Desc 1",
-      priority: "low",
-      status: "open",
-      createdAt: new Date().toISOString(),
-    },
-    {
-      id: 2,
-      title: "Test Ticket 2",
-      description: "Desc 2",
-      priority: "medium",
-      status: "open",
-      createdAt: new Date().toISOString(),
-    },
-  ];
-});
