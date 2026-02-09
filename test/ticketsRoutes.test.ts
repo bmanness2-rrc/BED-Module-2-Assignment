3755840f69eebb6a89b1b4db1fbbcce33f1088a2
Brody Manness <bmanness2@academic.rrc.ca>
2026-02-08 18:54:18 -0600

    Feat: Added proper mocks for routes.

--- test/ticketsRoutes.test.ts
+++ test/ticketsRoutes.test.ts
@@ -3,3 +3,14 @@ import app from "../src/app";
 import * as ticketControllers from "../src/api/v1/controllers/ticketsController";
 import * as urgencyController from "../src/api/v1/controllers/urgencyController";
 
+jest.mock("../src/api/v1/controllers/ticketsController", () => ({
+  getAllTicketsController: jest.fn((req, res) => res.status(200).send()),
+  getTicketByIdController: jest.fn((req, res) => res.status(200).send()),
+  createTicketController: jest.fn((req, res) => res.status(201).send()),
+  updateTicketController: jest.fn((req, res) => res.status(200).send()),
+  deleteTicketController: jest.fn((req, res) => res.status(200).send()),
+}));
+
+jest.mock("../src/api/v1/controllers/urgencyController", () => ({
+  getTicketUrgencyController: jest.fn((req, res) => res.status(200).send()),
+}));
